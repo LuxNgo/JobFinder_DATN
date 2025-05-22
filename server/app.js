5f300ff9264b52118bd7bb24c0e7a2188c3c6952
Ngo Luu <luu29103@gmail.com>
2025-05-22 16:14:59 +0700

    fix(feat/config): config deploy

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://job-finder-frontend.vercel.app",
     credentials: true,
   })
 );
